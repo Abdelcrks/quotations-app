01fcbcab7198aff218a4efb293eef108449243f3
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-04 11:58:35 +0200

    etape 5 terminé 😰

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ form.addEventListener("submit", (event) =>{
     const text =document.getElementById("citation-input").value
     const author =document.getElementById("author-input").value 
     console.log(text, author)
+    addQuote(text,author)
 })
+
+
+const addQuote = (text,author) => {
+    const paragraphText= document.createElement("p")
+        paragraphText.classList.add("text")
+        paragraphText.innerText = text
+    const paragraphAuthor= document.createElement("p")
+        paragraphAuthor.classList.add("author")
+        paragraphAuthor.innerText = author
+
+    const divQuote = document.createElement("div")
+    divQuote.classList.add("quote")
+    divQuote.appendChild(paragraphText)
+    divQuote.appendChild(paragraphAuthor)
+    document.getElementById("quote-list").appendChild(divQuote)
+}
